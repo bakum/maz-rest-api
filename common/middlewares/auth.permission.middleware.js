66aa977bc673eccb62cadfd3aff89d1daba6afe8
bakum <bakum.v@gmail.com>
2022-01-01 13:40:53 +0200

    orders

--- common/middlewares/auth.permission.middleware.js
+++ common/middlewares/auth.permission.middleware.js
@@ -34,7 +34,7 @@ exports.onlySameUserOrAdminCanDoThisAction = (req, res, next) => {
 exports.sameUserCantDoThisAction = (req, res, next) => {
     let userId = req.jwt.userId;
 
-    if (req.params.userId !== userId) {
+    if (parseInt(req.params.userId) !== userId) {
         return next();
     } else {
         return res.status(400).send();
