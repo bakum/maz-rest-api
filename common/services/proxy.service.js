1985f4b76486e2ff35b4548ebd66163c95ad1c71
bakum <bakum.v@gmail.com>
2022-01-07 23:16:30 +0200

    proxy.service.js

--- common/services/proxy.service.js
+++ common/services/proxy.service.js
@@ -9,7 +9,8 @@ exports.useProxyIfNeeded = (app) => {
     if (config.proxy.use) {
         app.use('/', createProxyMiddleware(filter, {
                     target: `${config.proxy.proxyEndpoint}:${config.proxy.port}/`,
-                    changeOrigin: true
+                    changeOrigin: true,
+                    ws: true
                 }
             )
         )
