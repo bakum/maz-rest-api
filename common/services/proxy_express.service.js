640ed3c60b94e2f59229c3fa64b516484dd900be
bakum <bakum.v@gmail.com>
2022-04-02 13:21:52 +0300

    404

--- common/services/proxy_express.service.js
+++ common/services/proxy_express.service.js
@@ -1,17 +1,16 @@
 const httproxy = require('express-http-proxy'),
     config = require('config'), errHundler = require('../errorHundler/error.hundler'),
-    api = config.get('api.uri'), proxy = config.get('proxy')
-
-const filterDjango = (req, res) => {
-    let apiPath = api.slice(0, -1), result
-    let isNotWebmin = req.headers.hasOwnProperty('referer') ? !req.headers['referer'].includes('/webmin') : req.url.includes('/webmin') ? !req.url.includes('/webmin') : true
-    result = !req.url.includes(apiPath) && isNotWebmin
-    return result
-}, djangoProxy = httproxy(`${proxy.proxyEndpoint}:${proxy.port}/`, {
-        filter: filterDjango,
-        limit: '10mb',
-    }
-)
+    api = config.get('api.uri'), proxy = config.get('proxy'),
+    filterDjango = (req, res) => {
+        let apiPath = api.slice(0, -1), result
+        let isNotWebmin = req.headers.hasOwnProperty('referer') ? !req.headers['referer'].includes('/webmin') : req.url.includes('/webmin') ? !req.url.includes('/webmin') : true
+        result = !req.url.includes(apiPath) && isNotWebmin
+        return result
+    }, djangoProxy = httproxy(`${proxy.proxyEndpoint}:${proxy.port}/`, {
+            filter: filterDjango,
+            limit: '10mb',
+        }
+    )
 
 exports.useProxyIfNeeded = (app) => {
     if (proxy.use) {
